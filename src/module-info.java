4d5f0bde3dc2465817480efd07af9160bd2d6481
Matias Laime <matulai1221@gmail.com>
2023-11-06 20:14:09 -0300

    cosas

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module module {
+	exports ar.edu.unq.po2.tpFinal.orden;
+	exports ar.edu.unq.po2.tpFinal.serviciosDeContainer;
+	exports ar.edu.unq.po2.tpFinal;
+	exports ar.edu.unq.po2.tpFinal.container;
+
+	requires org.junit.jupiter.api;
+}
