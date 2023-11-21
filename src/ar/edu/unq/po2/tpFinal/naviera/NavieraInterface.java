121572ef79571a11ce699bbe0b60bee846eb356a
Bautista Bracco <bautista.bracco@gmail.com>
2023-11-21 14:48:19 -0300

    Punto 5 (sin testearlo)

--- src/ar/edu/unq/po2/tpFinal/naviera/NavieraInterface.java
+++ src/ar/edu/unq/po2/tpFinal/naviera/NavieraInterface.java
@@ -1,4 +1,13 @@
 package ar.edu.unq.po2.tpFinal.naviera;
 
+import ar.edu.unq.po2.tpFinal.terminal.TerminalInterface;
+import ar.edu.unq.po2.tpFinal.viaje.Viaje;
+import ar.edu.unq.po2.tpFinal.viaje.ViajesInterface;
+
+import java.time.Duration;
+import java.util.List;
+
 public interface NavieraInterface {
+    Duration getTiempoDeViaje(TerminalInterface origen, TerminalInterface destino);
+    List<ViajesInterface> getViajes();
 }
